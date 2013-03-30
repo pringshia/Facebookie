0a1f8db9d256cb7640c0caecca50a4d7ab570171
pringshia <pratik@ringshia.com>
2013-03-30 17:35:32 -0400

    Initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Facebookie::Application.config.secret_token = '6e55caa4f59dcdb592fbccd2f53421b1d63b326f2a62c29f7a7f853f1596e88c3bf723007f863c140b9bde4e1941a78ea10d9b980c53226339ca790231bec5b8'
